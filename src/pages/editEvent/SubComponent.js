1929525634d028c9520b823e7a538441f7f54262
omahonynoel <46756671+omahonynoel@users.noreply.github.com>
2022-06-08 18:04:45 +0100

    Add files via upload

---
+++ src/pages/editEvent/SubComponent.js
@@ -0,0 +1,16 @@
+
+//import AccordionItem from "./AccordionItem";
+// styles
+//import styles from  '../../components/Accordion.module.css'
+
+
+
+export default function SubComponent({foo}) {
+
+
+
+  return (
+    <div>{foo}</div>
+  );
+};
+
